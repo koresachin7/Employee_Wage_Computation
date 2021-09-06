d5b1723c32f4b24bcb640d00b908b104e9ca1811
koresachin7 <koresachin7@gmail.com>
2021-09-06 14:41:06 +0530

    Added : Employee is present or absent chack

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,6 +3,15 @@ package com.bridgelabz;
 public class EmployeeWage { // Creating Employee Wage class
 
     public static void main(String[] args) {
+        //Constants
+        int IS_PRESENT = 1;
         System.out.println("Welcome to Employee Wage");
+        double echEmp = Math.floor(Math.random()* 10) %2;
+        // Using random class to Get random number
+        if (echEmp == IS_PRESENT){ //echEmp get 1 when first condition is true and showing employee present
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
